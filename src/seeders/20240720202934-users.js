41f0872ec1a2973eeb1e50d5284e2c5acd006c7e
Maxim Khlopyanik <Maxim Khlopyanik>
2024-07-20 23:52:25 +0300

    Добавлен сид и миграция

---
+++ src/seeders/20240720202934-users.js
@@ -0,0 +1,40 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert('users', [
+      {
+        first_name: 'John',
+        last_name: 'Doe',
+        email: 'John@example.com',
+        password: '12345',
+        role: 'ADMIN',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        first_name: 'Maik',
+        last_name: 'Joseph',
+        email: 'Maik@example.com',
+        password: '12345',
+        role: 'ADMIN',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        first_name: 'Doe',
+        last_name: 'Joseph',
+        email: 'Doe@example.com',
+        password: '12345',
+        role: 'ADMIN',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ]);
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete('users', null, {});
+  },
+};
+
